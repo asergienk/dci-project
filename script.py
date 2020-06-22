44ff2d29f0d780bbcb8fe4b66a66d7bfe596325a
Anna Sergienko <sergienkoaa1@gmail.com>
2020-06-22 18:28:09 -0400

    Added get_duration function

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ context = build_signature_context()
 
 #from the list of failed tasks takes the id of a task that failed first; takes job_id
 def get_failed_task_id(job_id):
-    r = dci_job.list_jobstates(context, id=job_id, where="status:failure", limit=1000, offset=0, embed='files')
+    r = dci_job.list_jobstates(context, id=job_id, where='status:failure', limit=1000, offset=0, embed='files')
     task_id = r.json()['jobstates'][-1]['files'][0]['id']
     return task_id
 
@@ -17,27 +17,26 @@ def get_failed_task_id(job_id):
 def get_failed_task_contents(task_id):
     res = dci_file.content(context, id=task_id)
     file_contents = res.text
-    print(file_contents)
+    #print(file_contents)
 
 
 #gets comment(failure stage) of the failed task
 def get_comment(job_id):
-    r = dci_job.list_jobstates(context, id=job_id, where="status:failure", limit=1000, offset=0, embed='files')
+    r = dci_job.list_jobstates(context, id=job_id, where='status:failure', limit=1000, offset=0, embed='files')
     comment = r.json()['jobstates'][-1]['comment']
     return comment
 
 
 
 
-# #gets duration of the job
-# def get_duration(job_id):
+#gets duration of the job in seconds and converting to minutes; on the dashboard this value is in minutes and trancated
+def get_duration(job_id):
+    sec_in_min = 60
+    r = dci_job.get(context, id=job_id)
+    duration_in_min = r.json()['job']['duration'] / sec_in_min
+    return duration_in_min
 
 
 
 
-
-
-# #writing to a file
-# with open('test.json', 'w') as f:
-#     json.dump(jobs, f, indent=2)
 
