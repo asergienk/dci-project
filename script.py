956d0612117c65c2dfce7f88d3a80756052b9498
Anna Sergienko <sergienkoaa1@gmail.com>
2020-06-22 17:58:04 -0400

    Added functions to get failed task id, content and comment

--- script.py
+++ script.py
@@ -1,17 +1,43 @@
 import json
 from dciclient.v1.api.context import build_signature_context
 from dciclient.v1.api import job as dci_job
+from dciclient.v1.api import file as dci_file
 
 context = build_signature_context()
 
 
-r = dci_job.list(context, where="status:failure", limit=20, offset=0, embed='topic,remoteci')
-print(r.text)
+#from the list of failed tasks takes the id of a task that failed first; takes job_id
+def get_failed_task_id(job_id):
+    r = dci_job.list_jobstates(context, id=job_id, where="status:failure", limit=1000, offset=0, embed='files')
+    task_id = r.json()['jobstates'][-1]['files'][0]['id']
+    return task_id
 
-jobs = r.json()['jobs']
-print(r.json()['_meta']['count'])
 
-print(len(jobs))
+#outputs the contents of the task failure; takes task_id from get_failed_task_id function
+def get_failed_task_contents(task_id):
+    res = dci_file.content(context, id=task_id)
+    file_contents = res.text
+    print(file_contents)
 
-print(json.dumps(jobs[0], indent=4, sort_keys=True))
+
+#gets comment(failure stage) of the failed task
+def get_comment(job_id):
+    r = dci_job.list_jobstates(context, id=job_id, where="status:failure", limit=1000, offset=0, embed='files')
+    comment = r.json()['jobstates'][-1]['comment']
+    return comment
+
+
+
+
+# #gets duration of the job
+# def get_duration(job_id):
+
+
+
+
+
+
+# #writing to a file
+# with open('test.json', 'w') as f:
+#     json.dump(jobs, f, indent=2)
 
--- todo.md
+++ todo.md
@@ -4,5 +4,50 @@
 - ignore remoteci.rc & job.json git ignore
 - create commit 
 - from Shub's document: duration, status, look at jos states
-- for each job we need list of the job states
+- for each job we need list of the job states(new, running, pre-run, post-run, success, failure, killed, error)
 
+- ignore vscode venv folders, json
+
+
+
+
+
+
+
+
+
+
+# Notes
+1. To filter all the jobs based on the product name (we need only RHEL) I need to access 'topic' key to get to the 'name' key. But in the list of valid keys there are no such keys
+{"valid_keys":
+["comment",
+"status",
+"topic_id_secondary",
+"remoteci_id",
+"state",
+"previous_job_id",
+"created_at",
+"tag",
+"updated_at",
+"update_previous_job_id
+team_id",
+"etag",
+"user_agent",
+"topic_id",
+"duration",
+"client_version",
+"id",
+"product_id"]}
+
+2. Listing endpoints: sort, limit, offset, where, embed
+
+3. from dciclient.v1.api import job as dci_job
+
+r = dci_job.list_jobstates(context, id="cfe77482-d424-49f3-bff7-d0ca46937d02", limit=1000, offset=0)
+
+jobs = r.json()
+
+print(jobs)
+
+for list_jobstates method for the particular job the output is: list_jobstates_all.json
+if we want to limit to some particular status, need to have where="status:failure"
