350041814fc897bf959ed6ed25afe89f396c9275
Anna Sergienko <sergienkoaa1@gmail.com>
2020-06-25 14:11:27 -0400

    Added get_product_id_by_name

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import json
 from dciclient.v1.api.context import build_signature_context
 from dciclient.v1.api import job as dci_job
 from dciclient.v1.api import file as dci_file
+from dciclient.v1.api import topic as dci_topic
 
 
 context = build_signature_context()
@@ -11,6 +12,37 @@ context = build_signature_context()
 parser = argparse.ArgumentParser()
 parser.add_argument("job_id", help="Failed Job ID")
 args = parser.parse_args()
+
+
+def get_product_id_by_name(product_name):
+    r = dci_topic.list(context, where=f"name:{product_name}")
+    product_id = r.json()["topics"][0]["product_id"]
+    return product_id
+
+ 
+def get_failed_job_ids(callback, product_id):
+    num_of_failed_jobs = dci_job.list(
+        context, where=f"product_id:{product_id},status:failure", limit=1, offset=0
+    ).json()["_meta"]["count"]
+    offset = 0
+    limit = 100
+    while offset < num_of_failed_jobs:
+        jobs_list = dci_job.list(
+            context,
+            where=f"product_id:{product_id},status:failure",
+            limit=limit,
+            offset=offset,
+        ).json()["jobs"]
+        for job in jobs_list:
+            callback(job)
+        offset += limit
+ 
+ 
+def print_to_csv(job):
+    job_id = job["id"]
+    with open("job_ids.csv", "a", newline="") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow([job_id])
 
 
 def get_first_failed_jobstate(job_id):
@@ -50,13 +82,15 @@ def get_remoteci_name(job_id):
     remoteci_name = r.json()["job"]["remoteci"]["name"]
     return remoteci_name
 
-
+product_name = "RHEL-8.2"
+product_id = get_product_id_by_name(product_name)
 first_failed_jobstate = get_first_failed_jobstate(args.job_id)
 failed_task_id = get_failed_task_id(first_failed_jobstate)
 failed_task_contents = get_failed_task_contents(failed_task_id)
 failed_comment = get_comment(first_failed_jobstate)
 failed_job_duration = get_duration(args.job_id)
 failed_remoteci_name = get_remoteci_name(args.job_id)
+get_failed_job_ids(print_to_csv, product_id)
 
 
 failed_task_contents_truncated = (
--- todo.md
+++ todo.md
@@ -1,12 +1,6 @@
 # to do
 
-- init a git repo 
-- ignore remoteci.rc & job.json git ignore
-- create commit 
-- from Shub's document: duration, status, look at jos states
-- for each job we need list of the job states(new, running, pre-run, post-run, success, failure, killed, error)
-- ignore vscode venv folders, json
-
+6/25/2020
 - create download_jobs_as_csv.py
 - grab the product_id by name https://github.com/redhat-cip/dci-downloader/blob/master/dci_downloader/api.py#L18-L27
 - commit and push 
@@ -21,28 +15,3 @@
 
 
 
-
-
-# Notes
-1. To filter all the jobs based on the product name (we need only RHEL) I need to access 'topic' key to get to the 'name' key. But in the list of valid keys there are no such keys
-{"valid_keys":
-["comment",
-"status",
-"topic_id_secondary",
-"remoteci_id",
-"state",
-"previous_job_id",
-"created_at",
-"tag",
-"updated_at",
-"update_previous_job_id
-team_id",
-"etag",
-"user_agent",
-"topic_id",
-"duration",
-"client_version",
-"id",
-"product_id"]}
-
-2. Listing endpoints: sort, limit, offset, where, embed
